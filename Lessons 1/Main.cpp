b52d2b49a7b49ec39b4410d4d85895e909bad6a1
Lassnochas <rud_dima97@mail.ru>
2017-02-07 16:16:25 +0200

    Add new line.

---
+++ Lessons 1/Main.cpp
@@ -0,0 +1,17 @@
+/*
+Name: Lessons 1-2-3
+Solutions: Learn c++
+Data Created: 07/02/2017
+Last edit:
+Author: Lassnochas
+Nenraiho Team | DEV
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	bool issue;
+	cout << "Hello My User, when i help you?";
+	system("pause");
+	return issue;
+}
